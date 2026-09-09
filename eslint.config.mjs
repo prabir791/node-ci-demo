3da429e540e8f6dfd48230fc55235ed5d16e715f
prabir791 <prabirrahman1@gmail.com>
2026-09-10 00:24:56 +0530

    Introduced lint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,23 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-{ files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.jest } } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      semi: ["error", "always"]
+    }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" }
+  }
 ]);
